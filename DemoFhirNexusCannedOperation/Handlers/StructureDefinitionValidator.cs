e4424271c21d25ae2d7c2706db7d3dca8f2895cb
Bruce Ye <bruce.ye@synapxe.sg>
2025-01-21 16:25:46 +0800

    Update with latest canned operations

---
+++ DemoFhirNexusCannedOperation/Handlers/StructureDefinitionValidator.cs
@@ -0,0 +1,31 @@
+﻿using Hl7.Fhir.Model;
+using Ihis.FhirEngine.Core;
+using Task = System.Threading.Tasks.Task;
+
+namespace DemoFhirNexusCannedOperation.Handlers
+{
+    [FhirHandlerClass(AcceptedType = nameof(StructureDefinition))]
+    public class StructureDefinitionValidator
+    {
+        [FhirHandler(HandlerCategory.PreCRUD, FhirInteractionType.Create)]
+        public async Task PreCRUD(IFhirContext fhirContext, CancellationToken cancellationToken = default)
+        {
+            fhirContext.Response.SetIsHandled(true);
+            fhirContext.Response.SetResourceResponse(new OperationOutcome
+            {
+                Issue = new List<OperationOutcome.IssueComponent>
+                {
+                    new OperationOutcome.IssueComponent
+                    {
+                        Severity = OperationOutcome.IssueSeverity.Information,
+                        Code = OperationOutcome.IssueType.Informational,
+                        Details = new CodeableConcept
+                        {
+                            Text = "All OK"
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
